f49b6decca5460d6bacbd99ceb96cb00ab73ac4b
seonjae2292@gmail.com <seonjae2292@gmail.com>
2025-05-13 22:43:55 +0900

    feat: 카카오 인가코드 받을 페이지 생성

---
+++ app/oauth/kakao/callback/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/router"
+
+const page = () => {
+    const searchParams = useSearchParams()
+    
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default page;
